f8645bcb043bd2b85388316de4175754573ae30a
zheng <yjmzzl123@163.com>
2022-06-19 19:43:15 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { return view('welcome'); });
-Route::get('/user', function () { return '我是user页面'; });
+Route::get('/user', function () { return view('welcome'); });

817a6a796ce21a1900dec4aeb6d39b5187a652f1
Your Name <1968186047@qq.com>
2022-07-05 23:44:00 +0800

    用户查询

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,10 +7,17 @@ use Illuminate\Http\Request;
 use App\Models\User;
 class UserController extends Controller
 {
+    /**
+     * 用户列表
+     */
     public function getUser(Request $request){
         try{    
             $data = User::all();
-            return $data;
+            return response()->json([ 
+                'code' => 200,
+                'msg' =>'获取列表成功',
+                'data' => $data,
+            ]);
         }catch(\Exception $e){
             return response()->json([ 
                 'code' => 100,
